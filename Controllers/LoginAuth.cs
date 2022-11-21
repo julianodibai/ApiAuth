74f5837b198308a20603cd968e2c957068977dfa
julianodibai <julianodibai2008@gmail.com>
2022-11-21 16:33:31 -0300

    Adicionando configurações auth

---
+++ Controllers/LoginAuth.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAuth.Controllers
+{
+    [ApiController]
+    [Route("/api/v1")]
+    public class LoginAuth : ControllerBase
+    {
+        [HttpGet]
+        //[Authorize]
+        [Route("login/{id}")]
+        public async Task<IActionResult> GetByIdAsync(long id)
+        {
+            return Ok();
+        }
+    }
+}
